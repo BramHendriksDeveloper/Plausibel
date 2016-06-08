820355a320989b6c60b4c4798e44c6af9bdc8a8f
Frank Kuipers <frank.k994@gmail.com>
2016-06-08 15:18:45 +0200

    Added Cirquit tests

--- UnitTestPlausibel/CircuitTest.cs
+++ UnitTestPlausibel/CircuitTest.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plausibel.Operator;
+using Plausibel.Parser;
+using Plausibel.Cirquit;
 
 namespace UnitTestPlausibel
 {
@@ -11,10 +14,88 @@ namespace UnitTestPlausibel
     public class CircuitTest
     {
 
-        public void CircuitTestX()
+        [TestMethod]
+        public void CircuitTestOne()
         {
+            // arrange
+            OperatorFactory opFactory = new OperatorFactory();
+            FileParser fParser = new FileParser();
+            CirquitCreator cCreator = new CirquitCreator(fParser);
+            Cirquit cirquit = cCreator.GetCirquit("one");
 
+            // act
+            Dictionary<string, bool> output = cirquit.Emulate(new Dictionary<string, bool> {
+                { "A", false },
+                { "B", false },
+                { "Cin", false },
+            });
+
+            // assert
+            Assert.AreEqual(false, output["S"]);
+            Assert.AreEqual(false, output["Cout"]);
         }
 
+        [TestMethod]
+        public void CircuitTestOne2()
+        {
+            // arrange
+            OperatorFactory opFactory = new OperatorFactory();
+            FileParser fParser = new FileParser();
+            CirquitCreator cCreator = new CirquitCreator(fParser);
+            Cirquit cirquit = cCreator.GetCirquit("one");
+
+            // act
+            Dictionary<string, bool> output = cirquit.Emulate(new Dictionary<string, bool> {
+                { "A", false },
+                { "B", true },
+                { "Cin", false },
+            });
+
+            // assert
+            Assert.AreEqual(true, output["S"]);
+            Assert.AreEqual(false, output["Cout"]);
+        }
+
+        [TestMethod]
+        public void CircuitTestOne3()
+        {
+            // arrange
+            OperatorFactory opFactory = new OperatorFactory();
+            FileParser fParser = new FileParser();
+            CirquitCreator cCreator = new CirquitCreator(fParser);
+            Cirquit cirquit = cCreator.GetCirquit("one");
+
+            // act
+            Dictionary<string, bool> output = cirquit.Emulate(new Dictionary<string, bool> {
+                { "A", false },
+                { "B", true },
+                { "Cin", true },
+            });
+
+            // assert
+            Assert.AreEqual(false, output["S"]);
+            Assert.AreEqual(true, output["Cout"]);
+        }
+
+        [TestMethod]
+        public void CircuitTestOne4()
+        {
+            // arrange
+            OperatorFactory opFactory = new OperatorFactory();
+            FileParser fParser = new FileParser();
+            CirquitCreator cCreator = new CirquitCreator(fParser);
+            Cirquit cirquit = cCreator.GetCirquit("one");
+
+            // act
+            Dictionary<string, bool> output = cirquit.Emulate(new Dictionary<string, bool> {
+                { "A", true },
+                { "B", true },
+                { "Cin", true },
+            });
+
+            // assert
+            Assert.AreEqual(true, output["S"]);
+            Assert.AreEqual(true, output["Cout"]);
+        }
     }
 }
